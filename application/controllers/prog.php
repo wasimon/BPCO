2c82bfa8b66ece7d95843bba64937a2655a0bb08
Esolitos <jj.69kt@gmail.com>
2014-03-12 16:56:20 +0100

    Aggiornamento Scelta Programma

--- application/controllers/prog.php
+++ application/controllers/prog.php
@@ -18,18 +18,33 @@ class Prog extends CI_Controller
 		$erg = $this->esami->getCicloERG($cf);
 		$antrop = $this->esami->getAntropometria($cf);
 		
-		echo "<pre>";
+		echo "<!-- \n";
 		print_r($subj);
 		print_r($erg);
 		print_r($antrop);
-		echo "</pre>";
+		echo "\n -->";
+		
+		$ratio = ($erg->vo2max / $erg->vo2predetto);
+		
+		echo "<br/>Ratio: {$ratio}";
+		
+		if ($ratio <= 0.25) {
+			$this->view_data['prog_choosen'] = 1;
+			
+		} elseif($ratio > 0.25 && $ratio <= 0.5) {
+			
+			$this->view_data['prog_choosen'] = 2;
+		} elseif ($ratio > 0.5 && $ratio <= 0.75) {
+			
+			$this->view_data['prog_choosen'] = 3;
+		} else {
+			
+			$this->view_data['prog_choosen'] = 4;
+		}
+		
+		echo "<br>Programma selezionato: {$this->view_data['prog_choosen']}";
+		
+		
 	}
 }       
-       
-       
-       
-       
-       
-       
-       
        
